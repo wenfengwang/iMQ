247804216209bb524de3b450d0ab396a41806ab0
wangwenfeng <sxian.wang@gmail.com>
2018-12-01 23:34:27 +0800

    add Topic &Subscription Interface

---
+++ client/topic.go
@@ -0,0 +1,31 @@
+package client
+
+import (
+	"github.com/wenfengwang/iMQ/broker/pb"
+	pb "github.com/wenfengwang/iMQ/client/pb"
+)
+
+
+
+type Topic interface {
+	Publish(*brokerpb.Message) pb.PublishResult
+	PublishBatch([]*brokerpb.Message) pb.PublishResult
+	PublishAsync(*brokerpb.Message, func(pb.PublishResult))
+	PublishBatchAsync([]*brokerpb.Message, func(pb.PublishResult))
+	Subscribe(string) Subscription
+}
+
+type Subscription interface {
+	Pull(int32) PullResult
+	Push(func([]*brokerpb.Message) pb.ConsumeResult)
+}
+
+type PullResult brokerpb.PullMessageResponse
+
+func (pr *PullResult) Ack() {
+	
+}
+
+func NewTopic(topicName string) Topic {
+	return nil
+}
